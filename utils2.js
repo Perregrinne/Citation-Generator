91b93a3ea6c3fe4282c353c3225c88c4e8cef15c
grahamIT <ajacob779@gmail.com>
2023-01-14 10:04:29 -0700

    Dark theme!
    With a few simple lines of code, I added a togglable dark theme. Prefers-color-scheme will detect users' preference. Also, radio buttons and JS will modify the form fields.

--- utils2.js
+++ utils2.js
@@ -79,13 +79,55 @@ function addAuthor()
     document.getElementById("author-list").appendChild(document.createElement("br"));
 }
 
+function updateMedia()
+{
+    document.dataset.medium = document.querySelector('input[name="medium"]:checked').value;
+    checkFields();
+}
+
+function updateFormat()
+{
+    document.dataset.format = document.querySelector('input[name="format"]:checked').value;
+    checkFields();
+}
+
 //I started having multiple things to update on page load,
 //so I like the idea of being able to add things here.
 function checkPage() {
-    checkFooter();
+    checkFields(); //Run before checkDOIInfo and checkURLInfo or those fields won't exist
+    checkThemeIcon();
     checkDOIInfo();
+    checkURLInfo();
+    checkFooter();
 }
 
+//By default, the theme toggle icon is the moon, but if users have "prefers-color-scheme"
+//set to "dark", then we need to update the icon:
+function checkThemeIcon()
+{
+    const color = document.body.style.backgroundColor;
+    const icon  = document.querySelector("#theme-icon");
+    
+    if(color === "rgb(241, 241, 241)")
+    {
+        icon.classList.add("bi-moon-fill");
+        icon.classList.remove("bi-sun-fill");
+    }
+    else
+    {
+        icon.classList.add("bi-sun-fill");
+        icon.classList.remove("bi-moon-fill");
+    }
+}
+
+function toggleTheme()
+{
+    const color = document.body.style;
+    color.backgroundColor = (color.backgroundColor === "rgb(241, 241, 241)") ? "rgb(26, 26, 26)" : "rgb(241, 241, 241)";
+    checkThemeIcon();
+}
+
+//Gives the DOI field a tooltip on hover:
 function checkDOIInfo() {
     const doiInfo = document.querySelector("#doi-info");
     const title = `DOI stands for "Document Object Identifier."
@@ -97,11 +139,117 @@ doi:10.0000/0000000000
 
 - or -
 
-http://dx.doi.org/10.0000/0000000000
+https://dx.doi.org/10.0000/0000000000
 
 Please copy/paste the entire DOI as you find it: with "doi:" or
-"http://dx.doi.org/" at the start of it, followed by the numbers!
+"https://dx.doi.org/" at the start of it, followed by the numbers!
 This app does not format the DOI in any special way, so make sure
 you copy and paste it into the text field exactly as it's given!`;
     doiInfo.setAttribute("title", title);
+}
+
+//Gives the URL field a tooltip on hover:
+function checkURLInfo() {
+    const urlInfo = document.querySelector("#url-info");
+    const title = `Don't bother with URL if you have the
+DOI already!
+
+Also, don't put a DOI URL in this field!`;
+    urlInfo.setAttribute("title", title);
+}
+
+function checkFields()
+{
+    const medium = document.body.dataset.medium;
+    const format = document.body.dataset.format;
+    const fields = document.querySelector("#swap");
+
+    if(medium === "book" && format === "apa")
+    {
+        fields.innerHTML = `<div class="row g-3 my-1">
+        <div class="col md-4">
+            <label for="input-first" class="my-1 text-nowrap">First Name</label>
+            <input type="text" class="form-control" id="input-first">
+        </div>
+        <div class="col md-4">
+            <label for="input-middle" class="my-1 text-nowrap">Middle Name</label>
+            <input type="text" class="form-control" id="input-middle">
+        </div>
+        <div class="col md-4">
+            <label for="input-last" class="my-1 text-nowrap">Last Name</label>
+            <input type="text" class="form-control" id="input-last">
+        </div>
+    </div>
+    <div class="row g-3">
+        <div class="col md-12">
+            <button class="btn btn-primary">+Author</button>
+        </div>
+    </div>
+    <div class="row g-3 my-1">
+        <div class="col md-4">
+            <label for="input-year" class="my-1 text-nowrap">Year Published</label>
+            <input type="number" class="form-control" id="input-year">
+        </div>
+        <div class="col md-4"></div>
+        <div class="col md-4"></div>
+    </div>
+    <div class="row g-3 my-1">
+        <div class="col md-4">
+            <label for="input-title" class="my-1">Title</label>
+            <input type="text" class="form-control" id="input-title">
+        </div>
+        <div class="col md-4">
+            <label for="input-edition" class="my-1 text-nowrap">Edition Number</label>
+            <input type="number" class="form-control" id="input-edition" min="1">
+        </div>
+        <div class="col md-4"></div>
+    </div>
+    <div class="row g-3 my-1">
+        <div class="col md-4">
+            <label for="input-desc" class="my-1 text-nowrap">Description (If No Title)</label>
+            <input type="text" class="form-control" id="input-desc">
+        </div>
+        <div class="col md-4"></div>
+        <div class="col md-4"></div>
+    </div>
+    <div class="row g-3 my-1">
+        <div class="col md-4">
+            <label for="input-publisher" class="my-1">Publisher</label>
+            <input type="text" class="form-control" id="input-publisher">
+        </div>
+        <div class="col md-4"></div>
+        <div class="col md-4"></div>
+    </div>
+    <div class="row g-3 my-1">
+        <div class="col md-4">
+            <label for="input-doi" class="my-1 text-nowrap">DOI <i class="bi-question-circle link-secondary" id="doi-info" title=""></i></label>
+            <input type="text" class="form-control" id="input-doi">
+        </div>
+        <div class="col md-4">
+            <label for="input-url" class="my-1 text-nowrap">URL (If Found Online) <i class="bi-question-circle link-secondary" id="url-info" title=""></i></label>
+            <input type="text" class="form-control" id="input-url">
+        </div>
+        <div class="col md-4"></div>
+    </div>`;
+    }
+    else if(medium == "web" && format === "apa")
+    {
+        fields.innerHTML = "";
+    }
+    else if(medium == "video" && format === "apa")
+    {
+        fields.innerHTML = "";
+    }
+    if(medium === "book" && format === "mla")
+    {
+        fields.innerHTML = "";
+    }
+    else if(medium == "web" && format === "mla")
+    {
+        fields.innerHTML = "";
+    }
+    else if(medium == "video" && format === "mla")
+    {
+        fields.innerHTML = "";
+    }
 }
